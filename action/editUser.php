e1019d774241f9998e7f87affa62077f2252116e
PJJunio <paulodjunior.dev@gmail.com>
2025-09-05 15:04:21 -0300

    iniciando editUser

---
+++ action/editUser.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once __DIR__ . '/../config/showErros.php';
+include_once __DIR__ . '/../config/dbConn.php';
+require_once __DIR__ . '/../database/Database.php';
+
+session_start();
+
+if (empty($_SESSION)) {
+    header('Location: view/login.php');
+    exit;
+}
+
+echo $_GET['id'];
